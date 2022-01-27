2c86990e4894e28e30421b38101d1161c69a8a55
stu2005 <konstu8@yahoo.co.jp>
2022-01-28 02:05:55 +0900

    fix entrypoint

--- chinachu/services.sh
+++ chinachu/services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chown node:node config.json rules.json 
 cd data
-chown node:node *
+chown node:node recorded.json  recording.json  reserves.json  schedule.json  scheduler.pid
 cd ..
 
 rm -f /var/run/chinachu-operator.pid > /dev/null 2>&1
